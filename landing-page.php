2ae1360ff96ca1f4677bf503b88eacb761ebe4fe
ahmetcingoz <ahmetarifcingoz@gmail.com>
2021-07-22 18:16:16 +0300

    font

--- landing-page.php
+++ landing-page.php
@@ -4,6 +4,7 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">	
+		<link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <link href="https://fonts.googleapis.com/css2?family=Life+Savers:wght@400;700;800&display=swap" rel="stylesheet">
 		
 		<link rel="stylesheet" type="text/css" href="style.css"/>
 	</head>
